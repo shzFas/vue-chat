8dccaf0b513d7b590ba580b34b0559c201daab8f
shzfas <yuraka200486@gmail.com>
2022-01-14 12:33:40 +0600

    Message

---
+++ src/components/Message.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="message">
+    <span v-if="!sender">@{{ name }}</span>
+    <div class="flex" :class="sender ? 'flex-row-reverse' : ''">
+      <Avatar class="mt-1" :src="photoUrl" />
+      <div class="text w-3/4" :class="sender ? 'bg-green-500' : 'bg-gray-300'"> <!-- фон клиента зеленый, фон собеседника белый -->
+        <slot />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Avatar from './Avatar.vue'
+export default {
+  components: { Avatar },
+  props: {
+    name: { type: String, default: '' },
+    photoUrl: { type: String, default: '' },
+    sender: { type: Boolean, default: false }
+  }
+}
+</script>
